9a2fa8f234e902c4a799647a7b00e1db10d1b789
guyunxiang <guyunxiang32@gmail.com>
2024-12-02 23:33:19 -0500

    update file import

--- src/components/components.ts
+++ src/components/components.ts
@@ -3,8 +3,8 @@ import { ComponentLoader } from 'adminjs'
 const componentLoader = new ComponentLoader()
 
 const Components = {
-  MyImage: componentLoader.add('MyImage', './my-image.tsx'),
-  Dashboard: componentLoader.add('Dashboard', './Dashboard.tsx'),
+  MyImage: componentLoader.add('MyImage', './my-image'),
+  Dashboard: componentLoader.add('Dashboard', './Dashboard'),
   // other custom components
 }
 
